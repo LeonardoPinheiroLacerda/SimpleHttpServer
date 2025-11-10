24bed765a41905d901ff32456c9a60aa47e3423d
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2025-11-10 17:39:43 -0300

    test: unit test for parse factory

--- src/test/java/br/com/leonardo/parser/factory/HttpRequestFactoryTest.java
+++ src/test/java/br/com/leonardo/parser/factory/HttpRequestFactoryTest.java
@@ -37,4 +37,34 @@ class HttpRequestFactoryTest {
 
     }
 
+    @Test
+    void shouldCreateHttpRequestWithEmptyBody() {
+
+        //Given
+        final String raw = """
+                GET /users HTTP/1.1
+                Host: localhost:8080
+                Accept: application/json
+                User-Agent: PostmanRuntime/7.26.8
+                Accept-Encoding: gzip, deflate, br
+                Connection: keep-alive
+                Cache-Control: no-cache\r\n\r\n\r\n\r\n
+                """;
+
+        //When
+        final HttpRequestData httpRequestData = HttpRequestFactory.fromRawRequest(raw);
+
+        //Then
+        Assertions
+                .assertThat(httpRequestData)
+                .extracting(
+                        r -> r.requestLine().uri(),
+                        r -> r.requestLine().method(),
+                        r -> r.requestLine().version(),
+                        r -> r.headers().size()
+                )
+                .containsExactly("/users", HttpMethod.GET, "HTTP/1.1", 6);
+
+    }
+
 }
